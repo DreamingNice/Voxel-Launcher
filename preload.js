791d7264501a84df6f1738eb71db76af8a728e39
DreamingNice <162009557+DreamingNice@users.noreply.github.com>
2026-01-18 21:50:44 +0300

    preload.js

--- preload.js
+++ preload.js
@@ -8,6 +8,17 @@ contextBridge.exposeInMainWorld("electronAPI", {
   launchMinecraft: (version, username, ramAllocation) => ipcRenderer.invoke("launch-minecraft", version, username, ramAllocation),
   openGameDirectory: () => ipcRenderer.invoke("open-game-directory"),
   getSystemRam: () => ipcRenderer.invoke("get-system-ram"),
+  getJavaInstallations: () => ipcRenderer.invoke("get-java-installations"),
+  
+  // Account management
+  msLogin: () => ipcRenderer.invoke("ms-login"),
+  addOfflineAccount: (username) => ipcRenderer.invoke("add-offline-account", username),
+  getAccounts: () => ipcRenderer.invoke("get-accounts"),
+  selectAccount: (identifier) => ipcRenderer.invoke("select-account", identifier),
+  removeAccount: (identifier) => ipcRenderer.invoke("remove-account", identifier),
+  refreshAccount: (refreshToken) => ipcRenderer.invoke("refresh-account", refreshToken),
+
+
   onDownloadProgress: (callback) => {
     ipcRenderer.on("download-progress", (event, percent) => callback(percent));
   },
@@ -20,4 +31,5 @@ contextBridge.exposeInMainWorld("electronAPI", {
   removeDownloadProgressListener: () => {
     ipcRenderer.removeAllListeners("download-progress");
   },
+  
 });
